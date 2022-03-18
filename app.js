17e7e02989f3b65208bfabcc27adde05d145d599
someguynamedkarlo <100094050+someguynamedkarlo@users.noreply.github.com>
2022-03-18 14:05:56 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ new fullpage ('#fullpage', {
     autoScrolling: true,
     navigation: true,
 })
-let s = 0
-while (s<1){
+
 if (screen.width <= 700) {
     document.location = "index2.html";
-}
-    s=s+1
 }
